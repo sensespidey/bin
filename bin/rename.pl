f69767d9935a71c1b5f960b68d3d6778000d641b
Derek Laventure <dlaventure@rnao.org>
2010-07-28 10:41:24 -0400

    added mass rename script

---
+++ bin/rename.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+# rename - Larry's filename fixer
+
+$op = shift or die "Usage: rename expr [files]\n";
+chomp(@ARGV = <STDIN>) unless @ARGV;
+for (@ARGV) {
+  $was = $_;
+  eval $op;
+  die $@ if $@;
+  rename($was,$_) unless $was eq $_;
+}
